18e6a179778a9cab6e0535a91b41479253b78e12
geyunxiang <geyunxiang_95@sina.com>
2017-11-04 09:23:22 +0800

    Add: test suits

---
+++ TestSuits.cpp
@@ -0,0 +1,9 @@
+//
+//  TestSuits.cpp
+//  rsa-encryption-xcode
+//
+//  Created by 葛云祥 on 11/04/2017.
+//  Copyright © 2017年 葛云祥. All rights reserved.
+//
+
+#include "TestSuits.hpp"
---
+++ TestSuits.hpp
@@ -0,0 +1,28 @@
+//
+//  TestSuits.hpp
+//  rsa-encryption-xcode
+//
+//  Created by 葛云祥 on 11/04/2017.
+//  Copyright © 2017年 葛云祥. All rights reserved.
+//
+
+#ifndef TestSuits_hpp
+#define TestSuits_hpp
+
+#include "BigInt.h"
+#include "myRSA.hpp"
+
+void test_increaseOne();
+void test_setValueString();
+void test_setValueInt();
+void test_additionOperator();
+void test_minusOperator();
+void test_multiplicationOperator();
+void test_divisionOperator();
+void test_moduloOperator();
+void test_comparisionOperator();
+void test_pow();
+void test_checkIfPrime();
+void test_getRandom();
+
+#endif /* TestSuits_hpp */
